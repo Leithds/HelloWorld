c11fb81833a4f8efa18983d84e8712ed3af06532
Leithds <154087236+Leithds@users.noreply.github.com>
2024-09-21 14:01:51 -0600

    First Commit

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+# Print Hello World
+
+def main():
+    helloWorld() # Call helloWorld function to print Hello World
+
+def helloWorld ():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
